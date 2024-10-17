d9da948d30c02cf46d18566d8152d9984014424c
Pedernera3 <agustinpederner3@gmail.com>
2024-10-17 19:30:49 -0300

    agregamos funcionalidad a productos y carrito

--- JS/script.js
+++ JS/script.js
@@ -1,42 +1,17 @@
-let items = [
-    {name:"Remera", price: 20000, stock: 7},
-    {name:"Buzo", price: 15000, stock: 12},
-    {name:"Pantalon", price: 25000, stock: 4},
-]
+const contenedorCard = document.getElementById("productos-container")
 
-let itemsJSON = JSON.stringify(items)
-
-localStorage.setItem("items", itemsJSON)
-
-let itemGuardado = localStorage.getItem("items")
-
-let itemArray = JSON.parse(itemGuardado)
-
-console.log(itemArray)
-
-items.sort((a,b)=>{
-    if (a.price>b.price)
-        return 1
-    if (a.price<b.price)
-        return -1
-    return 0 
-})
-
-console.log(items)
-
-let nombres = items.map(item => item.name)
-console.log(nombres)
-
-let precioTotal = items.reduce((total,item)=> total + item.price, 0)
-console.log ("Sumatoria de productos: ",precioTotal)
-
-document.addEventListener("DOMContentLoaded", function(){
-    let boton = document.getElementById ("boton_inicio")
-    let texto = document.getElementById ("titulo_inicio")
-    let parrafo = document.querySelector("p")
-
-    boton.addEventListener("click", function(){
-        texto.textContent = "¡Ingresaste a la Tienda!"
-        parrafo.textContent = "Muchas Gracias"
-    })
-})
+function crearCardProductos(productos){
+    productos.forEach(producto => {
+        const nuevaprenda = document.createElement("div")
+        nuevaprenda.classList = "card-producto"
+        nuevaprenda.innerHTML = `
+            <img src=${producto.img}>
+            <h3>${producto.nombre}</h3>
+            <p>$${producto.precio}</p>
+            <button>Agregar al carrito</button>
+        `
+        contenedorCard.appendChild(nuevaprenda)
+        nuevaprenda.getElementsByTagName("button")[0].addEventListener("click",()=> agregarAlCarrito(producto))
+    });
+}
+crearCardProductos(remeras)
---
+++ JS/stock.js
@@ -0,0 +1,117 @@
+const remeras = [
+    {
+        "id": 1,
+        "nombre": "Remera 1",
+        "img": "../assets/img/remera_01.png",
+        "cantidad": 1,
+        "precio": 16000,
+        "categoria": "remeras",
+        "stock": true
+    },
+    {
+        "id": 2,
+        "nombre": "Remera 2",
+        "img": "../assets/img/remera_02.png",
+        "cantidad": 1,
+        "precio": 18000,
+        "categoria": "remeras",
+        "stock": true
+    },
+    {
+        "id": 3,
+        "nombre": "Remera 3",
+        "img": "../assets/img/remera_03.png",
+        "cantidad": 1,
+        "precio": 14000,
+        "categoria": "remeras",
+        "stock": true
+    },
+    {
+        "id": 4,
+        "nombre": "Remera 4",
+        "img": "../assets/img/remera_04.png",
+        "cantidad": 1,
+        "precio": 20000,
+        "categoria": "remeras",
+        "stock": true
+    }
+];
+
+const stockBuzo = [
+    {
+        "id": 1,
+        "nombre": "Buzo 1",
+        "img": "../assets/img/buzo_01.png",
+        "cantidad": 1,
+        "precio": 26000,
+        "categoria": "buzos",
+        "stock": true
+    },
+    {
+        "id": 2,
+        "nombre": "Buzo 2",
+        "img": "../assets/img/buzo_02.png",
+        "cantidad": 1,
+        "precio": 29000,
+        "categoria": "buzos",
+        "stock": true
+    },
+    {
+        "id": 3,
+        "nombre": "Buzo 3",
+        "img": "../assets/img/buzo_03.png",
+        "cantidad": 1,
+        "precio": 32000,
+        "categoria": "buzos",
+        "stock": true
+    },
+    {
+        "id": 4,
+        "nombre": "Buzo 4",
+        "img": "../assets/img/buzo_04.png",
+        "cantidad": 1,
+        "precio": 30000,
+        "categoria": "buzos",
+        "stock": true
+    }
+];
+
+const stockPantalon = [
+    {
+        "id": 1,
+        "nombre": "Pantalon 1",
+        "img": "../assets/img/pantalon_01.png",
+        "cantidad": 1,
+        "precio": 46000,
+        "categoria": "pantalones",
+        "stock": true
+    },
+    {
+        "id": 2,
+        "nombre": "Pantalon 2",
+        "img": "../assets/img/pantalon_02.png",
+        "cantidad": 1,
+        "precio": 56000,
+        "categoria": "pantalones",
+        "stock": true
+    },
+    {
+        "id": 3,
+        "nombre": "Pantalon 3",
+        "img": "../assets/img/pantalon_03.png",
+        "cantidad": 1,
+        "precio": 52000,
+        "categoria": "pantalones",
+        "stock": true
+    },
+    {
+        "id": 4,
+        "nombre": "Pantalon 4",
+        "img": "../assets/img/pantalon_04.png",
+        "cantidad": 1,
+        "precio": 50000,
+        "categoria": "pantalones",
+        "stock": true
+    }
+];
+
